66fe83216c3beb413fe695a2ca755ac479de0a50
jhonyrdesouza <58796224+jhonyrdesouza@users.noreply.github.com>
2024-09-12 19:06:28 -0300

    feat: add task list component with context integration

---
+++ apps/client/src/components/task-list/index.tsx
@@ -0,0 +1,51 @@
+import { useContext } from 'react';
+import { TaskContext } from '../../contexts/task';
+import { Clipboard } from '../icons/clipboard';
+import {
+  NoTaskFoundContainer,
+  NoTaskFoundTextContainer,
+  TaskItemContainer,
+  TaskLabel,
+  TaskLabelContainer,
+  TaskListContainer,
+} from './styles';
+import { TaskItem } from './task-item';
+
+export const TaskList = () => {
+  const { tasks } = useContext(TaskContext);
+
+  const isCompleted = tasks.filter((task) => task.isCompleted);
+
+  return (
+    <TaskListContainer>
+      <TaskLabelContainer>
+        <TaskLabel $variant="primary">
+          Tarefas criadas<span>{tasks.length}</span>
+        </TaskLabel>
+
+        <TaskLabel $variant="secondary">
+          Concluídas
+          <span>
+            {isCompleted.length} de {tasks.length}
+          </span>
+        </TaskLabel>
+      </TaskLabelContainer>
+
+      <TaskItemContainer>
+        {tasks.map((task) => (
+          <TaskItem key={task.name} task={task} />
+        ))}
+      </TaskItemContainer>
+
+      {tasks.length === 0 && (
+        <NoTaskFoundContainer>
+          <Clipboard />
+          <NoTaskFoundTextContainer>
+            <h3>Você ainda não tem tarefas cadastradas</h3>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </NoTaskFoundTextContainer>
+        </NoTaskFoundContainer>
+      )}
+    </TaskListContainer>
+  );
+};
---
+++ apps/client/src/components/task-list/styles.ts
@@ -0,0 +1,119 @@
+import styled from 'styled-components';
+
+export const TaskListContainer = styled.div`
+  margin-top: 64px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export const TaskLabelContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TaskLabel = styled.h2<{ $variant: 'primary' | 'secondary' }>`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 14px;
+  font-weight: 700;
+  color: ${(props) => (props.$variant === 'primary' ? props.theme['violet-500'] : props.theme['violet-500'])};
+
+  span {
+    padding: 2px 8px;
+    border-radius: 999px;
+    color: ${({ theme }) => theme['gray-200']};
+    background-color: ${({ theme }) => theme['gray-400']};
+    font-size: 12px;
+    font-weight: 700;
+  }
+`;
+
+export const TaskItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export const TaskItemStyled = styled.div<{ $isCompleted?: boolean }>`
+  padding: 16px;
+  border-radius: 8px;
+  background-color: ${(props) => props.theme['gray-500']};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  border: ${(props) => (props.$isCompleted ? props.theme['gray-500'] : props.theme['gray-400'])};
+
+  h2 {
+    color: ${(props) => (props.$isCompleted ? props.theme['gray-300'] : props.theme['gray-100'])};
+    font-size: 14px;
+    text-decoration: ${(props) => (props.$isCompleted ? 'line-through' : 'none')};
+    line-clamp: 1;
+  }
+`;
+
+export const ToggleButton = styled.button<{ $isCompleted?: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 17px;
+  height: 17px;
+  border-radius: 999px;
+  border: 2px solid ${(props) => (props.$isCompleted ? props.theme['purple-800'] : props.theme['violet-500'])};
+  background-color: ${(props) => (props.$isCompleted ? props.theme['purple-800'] : 'transparent')};
+  transition: background-color 100ms ease;
+  cursor: pointer;
+
+  &:hover:not(:disabled) {
+    background-color: ${({ theme }) => theme['violet-500']};
+  }
+
+  &:disabled {
+    cursor: default;
+  }
+
+  svg {
+    opacity: ${(props) => (props.$isCompleted ? 100 : 0)};
+  }
+`;
+
+export const TrashButton = styled.button`
+  width: 24px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
+  border: none;
+  border-radius: 4px;
+  color: ${({ theme }) => theme['gray-300']};
+  transition: all 100ms ease;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme['gray-400']};
+    color: ${({ theme }) => theme['red-500']};
+  }
+`;
+
+export const NoTaskFoundContainer = styled.div`
+  height: 244px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 16px;
+
+  color: ${({ theme }) => theme['gray-300']};
+`;
+
+export const NoTaskFoundTextContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 4px;
+`;
