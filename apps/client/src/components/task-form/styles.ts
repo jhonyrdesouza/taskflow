816a64c69a6b157a68dadcfe5095a7f625cf3f4e
jhonyrdesouza <58796224+jhonyrdesouza@users.noreply.github.com>
2024-09-12 19:07:37 -0300

    feat: add TaskForm component for adding new tasks

---
+++ apps/client/src/components/task-form/styles.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 16px;
+  border-radius: 8px;
+  color: inherit;
+  background-color: ${({ theme }) => theme['gray-500']};
+  border: 1px solid ${({ theme }) => theme['gray-700']};
+
+  ::placeholder {
+    color: ${({ theme }) => theme['gray-300']};
+  }
+
+  &:focus,
+  &:focus-visible {
+    border: 1px solid ${({ theme }) => theme['purple-500']};
+  }
+`;
+
+export const Button = styled.button`
+  padding: 16px;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme['violet-700']};
+  color: inherit;
+  font-size: 14px;
+  font-weight: 700;
+  border: none;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  transition: background-color 100ms ease-in-out;
+
+  &:hover {
+    background-color: ${({ theme }) => theme['blue-500']};
+  }
+
+  svg {
+    margin-left: 8px;
+  }
+`;
