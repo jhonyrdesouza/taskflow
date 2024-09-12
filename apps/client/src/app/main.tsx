dfe448a853dd5cf12869837e4757b693a67172b0
jhonyrdesouza <58796224+jhonyrdesouza@users.noreply.github.com>
2024-09-12 20:43:55 -0300

    chore: fix import order to align with code style guidelines

--- apps/client/src/app/main.tsx
+++ apps/client/src/app/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
 import { AuthProvider } from '../contexts/auth';
 import { TaskProvider } from '../contexts/task';
+import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
